853b150cd47ec745f93173bfd506a29b9da8de3e
Alex Wysoczanski <awysoczanski@healthfirst.org>
2019-08-09 15:44:22 -0400

    MVC structure, nice!

---
+++ BarHop/Model/Bar.swift
@@ -0,0 +1,9 @@
+//
+//  Bar.swift
+//  BarHop
+//
+//  Created by Wysoczanski, Alex on 8/9/19.
+//  Copyright © 2019 Wysoczanski, Alex. All rights reserved.
+//
+
+import Foundation
---
+++ BarHop/Model/Patron.swift
@@ -0,0 +1,9 @@
+//
+//  Patron.swift
+//  BarHop
+//
+//  Created by Wysoczanski, Alex on 8/9/19.
+//  Copyright © 2019 Wysoczanski, Alex. All rights reserved.
+//
+
+import Foundation
